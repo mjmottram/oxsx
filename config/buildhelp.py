09f7c7eb5674223c74996187bb4d04c075bc5814
Matt Mottram <mjmottram@gmail.com>
2016-06-30 18:34:07 +0100

    fix to work on SL6

--- config/buildhelp.py
+++ config/buildhelp.py
@@ -44,32 +44,44 @@ def parse_user_config(filename, dependencies):
     '''
     Read user_config and update the relevant fields
     '''
+    compiler = None # Use default
     cparse = ConfigParser()
     cparse.read(filename)
     for dep_name in cparse.sections():
-        if not dep_name in dependencies.keys():
+
+        if dep_name == "g++":
+            try:
+                compiler = cparse.get(dep_name, 'binary_path')
+            except:
+                print('!! user_config specifies g++ without binary_path')
+                Exit(0)
+
+        elif dep_name in dependencies.keys():
+            # loop over options for that dependency
+            for opt in cparse.options(dep_name):
+                if opt in VALID_FIELDS:
+                    dependencies[dep_name].__setattr__(opt, cparse.get(dep_name, opt))
+                else:
+                    print "Unknown build option: {0} for dependency {1}".format(opt, dep_name)
+    
+            # should be installed in standard location - try to find it
+            if dependencies[dep_name].lib_path is None:
+                try:
+                    dependencies[dep_name].lib_path = find_library(dependencies[dep_name].libs.split()[0])
+                except ValueError:
+                    # hmm.
+                    pass
+    
+        else:
             print('!! user_config specifies unknown dependency "{0}"'.format(dep_name))
             Exit(0)
-        
-        # loop over options for that dependencies
-        for opt in cparse.options(dep_name):
-            if opt in VALID_FIELDS:
-                dependencies[dep_name].__setattr__(opt, cparse.get(dep_name, opt))
-            else:
-                print "Unknown build option: {0} for dependency {1}".format(opt, dep_name)
-
-        # should be installed in standard location - try to find it
-        if dependencies[dep_name].lib_path is None:
-            try:
-                dependencies[dep_name].lib_path = find_library(dependencies[dep_name].libs.split()[0])
-            except ValueError:
-                # hmm.
-                pass
+    return compiler
 
 def update_and_check_env(conf, dependencies):
     '''
     Update the build environment with the dependencies
     '''
+    # First check that C++11 support is enabled
     for dep in dependencies.values():
         if dep.header_path:
             conf.env.Append(CPPPATH = [dep.header_path])
@@ -139,3 +151,7 @@ def find_library(library_name):
             except:
                 pass
     raise ValueError
+
+
+def test_cpp11():
+    env.Default
